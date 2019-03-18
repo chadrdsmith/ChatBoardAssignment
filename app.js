3c0f2901b19b199c477a8f18f10676bf48f24cf3
Chad Smith <chadrdsmith@gmail.com>
2019-03-18 17:19:28 -0500

    second commit

--- app.js
+++ app.js
@@ -1,14 +1,40 @@
-const express = require ('express');
-var app = express();
+// Requirements
+var express = require ('express'),
+      mongoose = require('mongoose')
+      Comment = require('./models/message')
+      basicAuth = require('basic-auth');
+
 var handlebars = require('express-handlebars').create({
     defaultLayout:'main'});
 
+// Express Middleware    
+var app = express();
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-
 app.set('port', process.env.PORT || 3000);
+app.use(require('body-parser').urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 
+// Authorization Middleware
+app.use(function (req, res, next) {
+    var user = basicAuth(req);
+
+    if (!user || !user.name || !user.pass) {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+        res.status(401);
+        res.render('401');
+    }
+
+    if (user.name === 'chenry' && user.pass === 'test') {
+        next();
+    } else {
+        res.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+        res.status(401);
+        res.render('401');
+    }
+});
+
+// Routes
 app.get ('/', function(req, res) {
     res.render('index');
 });
@@ -26,6 +52,7 @@ app.use(function(err, req, res, next){
     res.send('500');
 });
 
+// Express Listening Port
 app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
